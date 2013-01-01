e4139ba4e86dac3d412e76c8275b2488c16ff3b7
ehewlett3 <ehewlett@gmail.com>
2012-12-31 21:00:51 -0800

    add choices
    allows for temporary addition of choices - need to add saving of
    choices to file

--- choose.py
+++ choose.py
@@ -1,6 +1,5 @@
 def displayPage(num):
 	print pages[num] # display page text
-	print("Choose now!")
 	currChoices = []
 	i=0 # display choices
 	j=0
@@ -11,13 +10,34 @@ def displayPage(num):
 			currChoices.append(choice[2])
 		i+=1
 	return currChoices
+	
+def addChoice(pageNum):
+	newChoiceText = raw_input("New choice: ")
+	i=0 # display pages
+	for page in pages:
+		print i+1, page
+		i+=1
+	newChoicePage = 9999
+	while newChoicePage > i:
+		newChoicePage = input("...leads to page #: ")
+	if newChoicePage == i:
+		addPage()
+	choices.append([pageNum, newChoiceText, newChoicePage-1])
 
+def addPage():
+	newPageText = raw_input("New page: ")
+	
 def main():
 	pageNum = 0
 	while True:
 		currChoices=displayPage(pageNum)
-		yourChoice=input()
-		pageNum=currChoices[yourChoice-1]
+		yourChoice=99
+		while yourChoice > len(currChoices):
+			yourChoice=input("Choose now! ")
+		if yourChoice==0:
+			addChoice(pageNum)
+		else:
+			pageNum=currChoices[yourChoice-1]
 
 f = open("pages.dat") # read pages
 pages = f.readlines()
